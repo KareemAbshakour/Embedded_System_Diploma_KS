5b6b0b3363501c250f6ce7e4d76ee0c3b4c93851
Kareem Abd El-Shakour <kareemabshakour@gmail.com>
2024-08-28 10:26:30 +0300

    Assignment 1 / Ex1,Ex2

--- C_Programming/Assignment_1/ex_1/main.c
+++ C_Programming/Assignment_1/ex_1/main.c
@@ -2,11 +2,15 @@
 
 int main()
 {
-	int x;
+	/**********  EX 1 *****************/
 	printf("C Programming\n");
-	printf("/************************/\n");
+
+	/**************** EX 2 ******************/
+	int x;
+	printf("/*********************************/\n");
 	printf("Enter an Integer: ");
 	fflush(stdout); fflush(stdin);
 	scanf("%d", &x);
 	printf("You Entered: %d", x);
+
 }
